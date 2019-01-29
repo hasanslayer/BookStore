d258daa03f60f9ee2a4b2a9058ccdd95f1e6bc75
hasanslayer <hasan-dr2010@hotmail.com>
2019-01-29 20:56:10 +0200

    Add Book Controller

---
+++ BookStore.WebUI/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Domain.Abstract;
+using BookStore.Domain.Entities;
+
+namespace BookStore.WebUI.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookRepository _repository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _repository = bookRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Books);
+        }
+    }
+}
