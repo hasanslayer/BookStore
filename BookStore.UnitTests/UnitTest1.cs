beb36d07835a815d8f0f6591f43f7bef832df004
hasanslayer <hasan-dr2010@hotmail.com>
2019-02-01 00:52:14 +0200

    Unit Test Can Paginate

--- BookStore.UnitTests/UnitTest1.cs
+++ BookStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookStore.Domain.Abstract;
+using BookStore.Domain.Entities;
+using BookStore.WebUI.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace BookStore.UnitTests
 {
@@ -7,8 +13,31 @@ namespace BookStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            //Arrange
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(b => b.Books).Returns(new Book[]
+            {
+                new Book() {ISBN = 1, Title = "Book1"},
+                new Book() {ISBN = 2, Title = "Book2"},
+                new Book() {ISBN = 3, Title = "Book3"},
+                new Book() {ISBN = 4, Title = "Book4"},
+                new Book() {ISBN = 5, Title = "Book5"}
+            });
+
+            BookController controller = new BookController(mock.Object);
+            controller.PageSize = 3;
+
+            //Act
+            IEnumerable<Book> resultBooks = (IEnumerable<Book>)controller.List(1).Model;
+
+            //Assert
+            Book[] bookArray = resultBooks.ToArray();
+            Assert.IsTrue(bookArray.Length == 3);
+            Assert.AreEqual(bookArray[0].Title, "Book1");
+            Assert.AreEqual(bookArray[1].Title, "Book2");
+            Assert.AreEqual(bookArray[2].Title, "Book3");
         }
     }
 }
--- BookStore.WebUI/Controllers/BookController.cs
+++ BookStore.WebUI/Controllers/BookController.cs
@@ -11,13 +11,23 @@ namespace BookStore.WebUI.Controllers
     public class BookController : Controller
     {
         private IBookRepository _repository;
+        public int PageSize = 4;
 
         public BookController(IBookRepository bookRepository)
         {
             _repository = bookRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int pageNo = 1)
+        {
+            return View(_repository.Books
+                .OrderBy(b => b.ISBN)
+                .Skip((pageNo - 1) * PageSize)
+                .Take(PageSize)
+            );
+        }
+
+        public ViewResult ListAll()
         {
             return View(_repository.Books);
         }
