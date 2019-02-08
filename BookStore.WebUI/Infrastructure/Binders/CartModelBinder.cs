7aa7e7abe1c5cce6e32cf27e2a55a85dbb2bc9fa
hasanslayer <hasan-dr2010@hotmail.com>
2019-02-08 14:57:42 +0200

    Create custom model binder

---
+++ BookStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using BookStore.Domain.Entities;
+
+namespace BookStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string SessionKey = "Cart";
+        public object BindModel(ModelBindingExecutionContext controllerContext, ModelBindingContext bindingContext)
+        {
+            // get cart from session
+            Cart cart = null;
+            if (controllerContext.HttpContext.Session != null)
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[SessionKey];
+            }
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                if (controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[SessionKey] = cart;
+                }
+            }
+            return cart;
+
+        }
+
+        bool IModelBinder.BindModel(ModelBindingExecutionContext modelBindingExecutionContext, ModelBindingContext bindingContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
