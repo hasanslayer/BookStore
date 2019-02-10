dafed64ef3ae9108d6a507c49c5a48d0581a008f
hasanslayer <hasan-dr2010@hotmail.com>
2019-02-10 21:55:42 +0200

    Add Checkout view

--- BookStore.Domain/Entities/ShippingDetails.cs
+++ BookStore.Domain/Entities/ShippingDetails.cs
@@ -12,7 +12,9 @@ namespace BookStore.Domain.Entities
         [Required(ErrorMessage = "The name is required")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Line 1 is required")]
+        [Display(Name = "Address Line 1")]
         public string Line1 { get; set; }
+        [Display(Name = "Address Line 2")]
         public string Line2 { get; set; }
         [Required(ErrorMessage = "City is required")]
         public string City { get; set; }
