550e8bbdea57127aaaa529287ec8fe2599270b86
hasanslayer <hasan-dr2010@hotmail.com>
2019-02-15 21:42:19 +0200

    Create account controller

---
+++ BookStore.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.WebUI.Infrastructure.Abstract;
+using BookStore.WebUI.Models;
+
+namespace BookStore.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAuthProvider _authProvider;
+
+        public AccountController(IAuthProvider authProvider)
+        {
+            _authProvider = authProvider;
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_authProvider.Authenticate(model.Username, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("","Incorrect Username/Password");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+
+
+        }
+    }
+}
