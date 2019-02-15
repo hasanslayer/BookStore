e32521fda6978310d38df4ec9c93dc78138bf374
hasanslayer <hasan-dr2010@hotmail.com>
2019-02-15 20:47:20 +0200

    Add index view for admin

---
+++ BookStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Domain.Abstract;
+
+namespace BookStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IBookRepository _repository;
+
+        public AdminController(IBookRepository repository)
+        {
+            _repository = repository;
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(_repository.Books);
+        }
+    }
+}
